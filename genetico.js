a7790148fd75751b46082706b55e4b16c4ee3e6e
Camilo Orocú <luiscamilooc@gmail.com>
2024-05-18 16:39:49 -0600

    Bases AG

---
+++ genetico.js
@@ -0,0 +1,26 @@
+const CANTIDAD_INDIVIDUOS = 20;
+const GENERACIONES = 200;
+const PORCENTAJE_MUTACIONES = 0.7;
+const PORCENTAJE_COMBINAR = 0.5;
+const PORCENTAJE_INDIVIDUOS_GENERACION = 0.20;
+
+
+/**
+ * Función que se encarga de generar la población inicial
+ */
+function seleccion(){
+
+
+}
+
+function mutar(individuo){
+
+}
+
+function combinar(individuo1, individuo2){
+
+}
+
+function fitness(individuo){
+
+}
