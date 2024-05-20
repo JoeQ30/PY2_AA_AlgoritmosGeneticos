030c3a8d102f4b947e0a47687700f838dc17730f
Camilo Orocú <luiscamilooc@gmail.com>
2024-05-19 21:07:09 -0600

    Comentando codigo

--- genetico.js
+++ genetico.js
@@ -25,8 +25,12 @@ function fitness(individuo){
 
 }
 
+
 /**
- *  Funcion que genera un punto de forma aleatorio entre 
+ * Funcion que genera un punto de forma aleatorio en la imagen correspondiente al ancho y alto de la imagen
+ * 
+ * @param {imageWidth} maxX 
+ * @param {imageHeight} maxY 
  * @returns Punto aleatorio
  */
 function generarPuntoAleatorio(maxX, maxY){
@@ -37,6 +41,10 @@ function generarPuntoAleatorio(maxX, maxY){
 
 }
 
+/**
+ * Funcion que genera un color aleatorio de OpenCV
+ * @returns Color aleatorio
+ */
 function randomColor() {
   let r = Math.floor(Math.random() * 256);
   let g = Math.floor(Math.random() * 256);
@@ -45,7 +53,7 @@ function randomColor() {
 }
 
 /**
- * Función que se encarga de cargar los triangulos
+ * Función que se encarga de cargar la imagen y realizar el proceso de triangulación
  */
 function cargar(){
   let imgElement = document.getElementById("imageSrc")
@@ -89,36 +97,12 @@ function cargar(){
 
 
   };
-  var Module = {
-    // https://emscripten.org/docs/api_reference/module.html#Module.onRuntimeInitialized
-    onRuntimeInitialized() {
-      document.getElementById('status').innerHTML = 'OpenCV.js is ready.';
-    }
-
-
-  }
 }
-/*
-function onOpenCvReady(){
-    let mat = cv.imread(imgElement);
-    console.log(mat.ucharPtr(100,100))
-
-    let p1  = new cv.Point(0, 0);
-    let p2  = new cv.Point(20, 20);
-    let p3  = new cv.Point(10, 0);
-    let p4 = new cv.Point(65, 65);
-    let p5 = new cv.Point(200, 200);
+var Module = {
+  // https://emscripten.org/docs/api_reference/module.html#Module.onRuntimeInitialized
+  onRuntimeInitialized() {
+    document.getElementById('status').innerHTML = 'OpenCV.js is ready.';
+  }
 
 
-    let indiv = new cv.Mat.zeros(255, 255, cv.CV_8U);
-    cv.line(indiv, p1, p2, [255, 255, 255, 255], 1)
-    cv.line(indiv, p2, p3, [255, 255, 255, 255], 1)
-    cv.line(indiv, p3, p4, [255, 0, 0, 255], 2)
-    cv.line(indiv, p4, p5, [255, 255, 0, 255], 3)
-    
-    
-
-    // Mostrar la imagen
-    cv.imshow('canvasOutput', indiv);
-  };
-  */
+}
